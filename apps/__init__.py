97a18f6a2eab4a9a067a5723d3282560818db4bd
Sharma Nadakuditi <sharma.nadakuditi@ericsson.com>
2016-05-04 21:03:21 -0500

    Partv

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,4 +5,14 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
+
 from apps import views, models
